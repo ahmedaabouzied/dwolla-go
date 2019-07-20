f112cf5a10fee2c45216277c6693b9396d1d9452
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-07-20 14:44:14 +0200

    add funding api methods
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- dwolla/funding/funding.go
+++ dwolla/funding/funding.go
@@ -34,6 +34,33 @@ type ListResourcesResponse struct {
 	Embedded map[string][]Resource  `json:"_embedded"`
 }
 
+// VerifyMicroDepositsRequest is the request to verify microdeposits
+type VerifyMicroDepositsRequest struct {
+	Amount1 *Amount `json:"amount1"`
+	Amount2 *Amount `json:"amount2"`
+}
+
+// Amount is the amount part of a balance.
+type Amount struct {
+	Value    string `json:"value"`
+	Currency string `json:"currency"`
+}
+
+// BalanceResponse has the fields the describe the balance in a funding source.
+type BalanceResponse struct {
+	Links   map[string]map[string]client.Link `json:"_links"`
+	Total   Amount                            `json:"total"`
+	Balance Amount                            `json:"balance"`
+}
+
+// MicroDepositsDetails has the details for a microdeposits and their status.
+type MicroDepositsDetails struct {
+	Links     map[string]map[string]client.Link `json:"_links"`
+	CreatedAt string                            `json:"created"`
+	Status    string                            `json:"status"`
+	Failure   map[string]string                 `json:"failure"`
+}
+
 // GetFundingSource retrieves a funding source by id.
 func GetFundingSource(c *client.Client, sourceID string) (*Resource, error) {
 	hc := &http.Client{}
@@ -82,6 +109,7 @@ func (f *Resource) Update(c *client.Client) error {
 	}
 	req.Header.Add("Authorization", "Bearer "+token)
 	req.Header.Add("Accept", "application/vnd.dwolla.v1.hal+json")
+	req.Header.Add("Content-Type", "application/vnd.dwolla.v1.hal+json")
 	res, err := hc.Do(req)
 	if err != nil {
 		return errors.Wrap(err, "failed to make request to dwolla api")
@@ -128,4 +156,116 @@ func (f *Resource) IntiateMicroDeposits(c *client.Client) error {
 	default:
 		return errors.New(res.Status)
 	}
+}
+
+// VerifyMicroDeposits bank verification.
+func (f *Resource) VerifyMicroDeposits(c *client.Client, vr *VerifyMicroDepositsRequest) error {
+	hc := &http.Client{}
+	token, err := c.AuthToken()
+	if err != nil {
+		return errors.Wrap(err, "failed to get auth token")
+	}
+	body, err := json.Marshal(vr)
+	if err != nil {
+		return errors.Wrap(err, "error marshalling verify micro deposits")
+	}
+	req, err := http.NewRequest("POST", f.Links["self"].Href+"/micro-deposits", bytes.NewReader(body))
+	if err != nil {
+		return errors.Wrap(err, "error creating the request")
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Accept", "application/vnd.dwolla.v1.hal+json")
+	req.Header.Add("Content-Type", "application/vnd.dwolla.v1.hal+json")
+	res, err := hc.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "failed to make request to dwolla api")
+	}
+	defer res.Body.Close()
+	switch res.StatusCode {
+	case 200:
+		return nil
+	case 202:
+		return errors.New("Micro-deposits have not have not settled to destination bank. A Customer can verify these amounts after micro-deposits have processed to their bank")
+	case 403:
+		return errors.New("too many attempts, bank already verified")
+	case 404:
+		return errors.New("micro deposits not initiated or funding source not found")
+	case 500:
+		return errors.New("verify micro-deposits returned an unknown error")
+	default:
+		return errors.New(res.Status)
+	}
+}
+
+// GetBalance retrieves balance for the funding source.
+func (f *Resource) GetBalance(c *client.Client) (*BalanceResponse, error) {
+	hc := &http.Client{}
+	token, err := c.AuthToken()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get auth token")
+	}
+	req, err := http.NewRequest("GET", f.Links["self"].Href+"/balance", nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating the request")
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Accept", "application/vnd.dwolla.v1.hal+json")
+	res, err := hc.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request to dwolla api")
+	}
+	defer res.Body.Close()
+	switch res.StatusCode {
+	case 200:
+		d := json.NewDecoder(res.Body)
+		body := &BalanceResponse{}
+		err = d.Decode(body)
+		return body, nil
+	case 404:
+		return nil, errors.New("funding source not found")
+	default:
+		return nil, errors.New(res.Status)
+	}
+}
+
+// GetMicroDepositsDetails retrieves the status of micro-deposits
+// and checks if they are eligible for verification.
+func (f *Resource) GetMicroDepositsDetails(c *client.Client) (*MicroDepositsDetails, error) {
+	hc := &http.Client{}
+	token, err := c.AuthToken()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get auth token")
+	}
+	req, err := http.NewRequest("GET", f.Links["self"].Href+"/micro-deposits", nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating the request")
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Accept", "application/vnd.dwolla.v1.hal+json")
+	res, err := hc.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request to dwolla api")
+	}
+	defer res.Body.Close()
+	switch res.StatusCode {
+	case 200:
+		d := json.NewDecoder(res.Body)
+		body := &MicroDepositsDetails{}
+		err = d.Decode(body)
+		return body, nil
+	case 404:
+		return nil, errors.New("micro-depostis not found or have already been verified")
+	default:
+		return nil, errors.New(res.Status)
+	}
+}
+
+// Remove a funding resource.
+func (f *Resource) Remove(c *client.Client) error {
+	f.Removed = true
+	err := f.Update(c)
+	if err != nil {
+		return errors.Wrap(err, "error removing funding source")
+	}
+	return nil
 }
--- dwolla/funding/funding_test.go
+++ dwolla/funding/funding_test.go
@@ -14,7 +14,114 @@ func TestGetFundingSource(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	_, err = GetFundingSource(client, "49dbaa24-1580-4b1c-8b58-24e26656fa31")
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("sourceID", source.ID)
+}
+
+func TestUpdateFundingSource(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	source.Name = "Bank of Brgle"
+	err = source.Update(client)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInitiateMicroDeposits(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	err = source.IntiateMicroDeposits(client)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestVerifyMicroDeposits(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	vr := &VerifyMicroDepositsRequest{
+		Amount1: &Amount{
+			Value:    "0.03",
+			Currency: "USD",
+		},
+		Amount2: &Amount{
+			Value:    "0.03",
+			Currency: "USD",
+		},
+	}
+	err = source.VerifyMicroDeposits(client, vr)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetBalance(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = source.GetBalance(client)
+	if err != nil {
+		t.Error(err)
+	}
+}
+func TestGetMicroDepositsDetails(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = source.GetMicroDepositsDetails(client)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	gotenv.Load("../../.env")
+	client, err := client.CreateClient("sandbox", os.Getenv("DWOLLA_PUBLIC_KEY"), os.Getenv("DWOLLA_SECRET_KEY"))
+	if err != nil {
+		t.Error(err)
+	}
+	source, err := GetFundingSource(client, "5d2776f0-ba12-45ea-a7da-b4b01fa95ace")
+	if err != nil {
+		t.Error(err)
+	}
+	err = source.Remove(client)
 	if err != nil {
 		t.Error(err)
 	}
