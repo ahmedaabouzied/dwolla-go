d8d4cfc6b3a7aa792bc2473219ee883016cbb550
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2019-07-21 11:50:28 +0200

    update travis
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

---
+++ doc/doc.go
@@ -0,0 +1,11 @@
+// Packge dwolla is a third party client library for Dwolla REST API.
+//
+// The package provides APIs and utilites to be used do develop Go applications
+// with Dwolla payment services such as customers, transfers, masspayments , ...etc.
+//
+// The library removes the complexity of calling the endpoints directly from the application.
+// It handles some things like creating and maintaining the dwolla access token that expires 
+// after one hour.
+//
+// For more info and to understand Dwolla payment services.
+// https://docs.dwolla.com/
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Dwolla-go
+
 A Go client library for Dwolla rest API.
 [![GoDoc](https://godoc.org/github.com/ahmedaabouzied/dwolla-go?status.svg)](https://godoc.org/github.com/ahmedaabouzied/dwolla-go)
